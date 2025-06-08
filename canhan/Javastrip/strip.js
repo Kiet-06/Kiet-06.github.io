1d5fe07563af88a158b9e18c338e8715d61083a6
nguyenanhkiet <nguyenanhkiet130406@gmail.com>
2025-06-08 21:35:00 +0700

    Add files via upload

--- canhan/Javastrip/strip.js
+++ canhan/Javastrip/strip.js
@@ -1,1 +1,4 @@
-
+document.getElementById('viewProfile').addEventListener('click', function () {
+  // Chuyển hướng đến trang hồ sơ
+  window.location.href = '../css/hoso.html'; // Hoặc gắn link Google Drive, GitHub, PDF
+});
